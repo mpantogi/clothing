d46398f8ef4d2b0dc847d6c21ec1ff59719a2a85
mpantogi <48733545+mpantogi@users.noreply.github.com>
2021-10-31 20:38:10 +0200

    trying to fetch data from firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,17 +2,17 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-const config = {
-    apiKey: "AIzaSyDPECiqX9Ic7XgXoAatV4hdmpFv97Z6ktM",
-    authDomain: "crow-db-c2c0e.firebaseapp.com",
-    projectId: "crow-db-c2c0e",
-    storageBucket: "crow-db-c2c0e.appspot.com",
-    messagingSenderId: "99825839139",
-    appId: "1:99825839139:web:7648179be4996d86049596",
-    measurementId: "G-S918DMDNXE"
+const firebaseConfig = {
+  apiKey: "AIzaSyDPECiqX9Ic7XgXoAatV4hdmpFv97Z6ktM",
+  authDomain: "crow-db-c2c0e.firebaseapp.com",
+  projectId: "crow-db-c2c0e",
+  storageBucket: "crow-db-c2c0e.appspot.com",
+  messagingSenderId: "99825839139",
+  appId: "1:99825839139:web:7648179be4996d86049596",
+  measurementId: "G-S918DMDNXE"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -37,6 +37,42 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+
+
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
